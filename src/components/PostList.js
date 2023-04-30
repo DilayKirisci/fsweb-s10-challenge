a553e4523b66f749d4d96f5024917de1f3c6b696
Dilay Kirisci <isik.kirisci@gmail.com>
2023-04-30 21:04:21 +0300

    localstorage calismiyor

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Post from "./Post";
+import { useSelector } from "react-redux";
 
 const PostList = () => {
-  const notlar = [];
+	const notlar = useSelector((store) => store.notlar);
 
-  return notlar.length === 0 ? (
-    <div className="beyazKutu text-center p-6">Hiç notunuz yok</div>
-  ) : (
-    <div>
-      {notlar.map((not) => (
-        <Post item={not} key={not.id} />
-      ))}
-    </div>
-  );
+	return notlar.length === 0 ? (
+		<div className="beyazKutu text-center p-6">Hiç notunuz yok</div>
+	) : (
+		<div>
+			{notlar.map((not) => (
+				<Post item={not} key={not.id} />
+			))}
+		</div>
+	);
 };
 
 export default PostList;
--- src/reducers.js
+++ src/reducers.js
@@ -1,29 +1,54 @@
-const s10chLocalStorageKey = "s10ch";
+import { NOT_EKLE, NOT_SIL } from "./actions";
+
+const key = "s10ch";
 
 const baslangicDegerleri = {
-  notlar: [
-    {
-      id: "75g1IyB8JLehAr0Lr5v3p",
-      date: "Fri Feb 03 2023 09:40:27 GMT+0300 (GMT+03:00)",
-      body: "Bugün hava çok güzel!|En iyi arkadaşımın en iyi arkadaşı olduğumu öğrendim :)|Kedim iyileşti!",
-    },
-  ],
+	notlar: [
+		{
+			id: "75g1IyB8JLehAr0Lr5v3p",
+			date: "Fri Feb 03 2023 09:40:27 GMT+0300 (GMT+03:00)",
+			body: "Bugün hava çok güzel!|En iyi arkadaşımın en iyi arkadaşı olduğumu öğrendim :)|Kedim iyileşti!",
+		},
+	],
 };
 
 function localStorageStateYaz(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+	localStorage.setItem(key, JSON.stringify(data));
 }
 
 function localStorageStateOku(key) {
-  return JSON.parse(localStorage.getItem(key));
+	return JSON.parse(localStorage.getItem(key));
 }
 
-function baslangicNotlariniGetir(key) {
-  const eskiNotlar = localStorage.getItem(key);
+export function baslangicNotlariniGetir(key) {
+	const eskiNotlar = localStorage.getItem(key);
 
-  if (eskiNotlar) {
-    return localStorageStateOku(key);
-  } else {
-    return baslangicDegerleri
-  }
+	if (eskiNotlar) {
+		return localStorageStateOku(key);
+	} else {
+		return baslangicDegerleri;
+	}
+}
+
+export function reducer(state = baslangicDegerleri, action) {
+	switch (action.type) {
+		case NOT_EKLE:
+			const updatedState = {
+				...state,
+				notlar: [...state.notlar, JSON.parse(action.payload)],
+			};
+			localStorageStateYaz(key, updatedState);
+			return updatedState;
+
+		case NOT_SIL:
+			console.log(action.payload);
+			const removedState = {
+				...state,
+				notlar: state.notlar.filter((not) => not.id !== action.payload),
+			};
+			localStorageStateYaz(key, removedState);
+			return removedState;
+		default:
+			return state;
+	}
 }
